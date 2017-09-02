2330a85d3b4b3f8c0aa1fad0c3ba480cb6c3fba4
David L Cervantes <pakawaka1@gmail.com>
2017-09-02 12:55:36 -0700

    added input boxes and submit button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let body = document.querySelector('body');
+body.className = 'container';
+
 console.log(body);
 
 
-let ul = document.createElement('ul'),
+let ul1 = document.createElement('ul'),
     li1 = document.createElement('li'),
     a1 = document.createElement('a'),
     li2 = document.createElement('li'),
@@ -10,8 +12,7 @@ let ul = document.createElement('ul'),
     li3 = document.createElement('li'),
     a3 = document.createElement('a');
 
-console.log(ul);
-console.log(li1, li2, li3);
+
 
 a1.href ="https://www.facebook.com/";
 a2.href ="https://www.facebook.com/";
@@ -25,34 +26,31 @@ a3.innerHTML = 'projects';
 li1.append(a1);
 li2.append(a2);
 li3.append(a3);
-ul.append(li1, li2, li3);
-body.append(ul);
+ul1.append(li1, li2, li3);
 
-let form1 = document.createElement('form'),
-    form2 = docuument.creatElement('form'),
-    form3 = document.createElement('form');
+let li4 =document.createElement('li');
 
-console.log(form1, form2, form3);
-
-form.lastElementChild.addEventListener('click', function(e){
-    e.preventDefault();
-    if(form1.username.value){
-        console.log('Welcome ' + sForm.username.value);
-    }
-    if(form2.password.value){
-        console.log('Your password ' + sForm.password.value);
-    }
-});
+let input1 = document.createElement('input');
+input1.id = 'text1';
+let input2 = document.createElement('input');
+input2.id = 'text2';
+let btn1 = document.createElement('button');
 
 
+/*function myfunction() {
+  document.getElementById('text1').setAttribute('placeholder', document.getElementById('text1').getAttribute("username"));
+  document.getElementById('text2').setAttribute('placeholder', document.getElementById('text2').getAttribute("password"));
+}
+myfunction();*/
 
-
-
-body.className = 'container';
+//**Still having trouble creating a placeholder!******
 
 
 
 
-console.log(body);
+btn1.innerHTML = 'go';
 
-//create links
+
+li4.append(input1, input2, btn1);
+ul1.append(li4);
+body.append(ul1);
