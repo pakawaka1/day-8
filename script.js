07cdc910f28afd39b9f6c2423a902c038abf1cf3
David L Cervantes <pakawaka1@gmail.com>
2017-09-04 18:14:16 -0700

    finished javascript for form

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ body.className = 'container';
 
 console.log(body);
 
-
 let ul1 = document.createElement('ul'),
     li1 = document.createElement('li'),
     a1 = document.createElement('a'),
@@ -12,12 +11,9 @@ let ul1 = document.createElement('ul'),
     li3 = document.createElement('li'),
     a3 = document.createElement('a');
 
-
-
 a1.href ="https://www.facebook.com/";
 a2.href ="https://www.facebook.com/";
 a3.href ="https://www.facebook.com/";
-
 
 a1.innerHTML = 'home';
 a2.innerHTML ='about';
@@ -28,29 +24,21 @@ li2.append(a2);
 li3.append(a3);
 ul1.append(li1, li2, li3);
 
+let form1 = document.createElement('form');
+
 let li4 =document.createElement('li');
 
 let input1 = document.createElement('input');
+input1.placeholder = 'username';
 input1.id = 'text1';
 let input2 = document.createElement('input');
+input2.placeholder = 'password';
 input2.id = 'text2';
-let btn1 = document.createElement('button');
+let input3 = document.createElement('input');
+input3.type = 'submit';
+input3.value = 'go';
 
-
-/*function myfunction() {
-  document.getElementById('text1').setAttribute('placeholder', document.getElementById('text1').getAttribute("username"));
-  document.getElementById('text2').setAttribute('placeholder', document.getElementById('text2').getAttribute("password"));
-}
-myfunction();*/
-
-//**Still having trouble creating a placeholder!******
-
-
-
-
-btn1.innerHTML = 'go';
-
-
-li4.append(input1, input2, btn1);
+form1.append(input1, input2, input3);
+li4.append(form1);
 ul1.append(li4);
 body.append(ul1);
